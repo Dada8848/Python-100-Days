f50ccaf9e9ada5f950fe5f50cc4a7bc6ef676d10
Dada8848 <daming.zhang2016@gmail.com>
2022-09-18 09:03:03 +0800

    修改 测试

--- Day01-15/Day05/lily.py
+++ Day01-15/Day05/lily.py
@@ -11,7 +11,8 @@ Date: 2018-03-02
 """
 
 for num in range(100, 1000):
-	low = num % 10
+	low = int(str(num)[2])
+	# low = num % 10
 	mid = num // 10 % 10
 	high = num // 100
 	if num == low ** 3 + mid ** 3 + high ** 3:
