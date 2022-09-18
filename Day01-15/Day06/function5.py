a6de017c5cd3a5ddfdb861b896b6a835d6799c47
Dada8848 <daming.zhang2016@gmail.com>
2022-09-18 21:56:27 +0800

    修改 测试

--- Day01-15/Day06/function5.py
+++ Day01-15/Day06/function5.py
@@ -40,15 +40,18 @@ print(f2())
 # 关键字参数
 def f3(**kw):
 	if 'name' in kw:
-		print('欢迎你%s!' % kw['name'])
+		print('欢迎你,%s!' % kw['name'])
 	elif 'tel' in kw:
 		print('你的联系电话是: %s!' % kw['tel'])
+	elif 'age' in kw:
+		print('你已经%d岁了。' % kw['age'])
 	else:
 		print('没找到你的个人信息!')
 
 
-param = {'name': '骆昊', 'age': 38}
+param = {'name': '骆昊', 'age': 38, 'tel': '58467892'}
 f3(**param)
 f3(name='骆昊', age=38, tel='13866778899')
 f3(user='骆昊', age=38, tel='13866778899')
 f3(user='骆昊', age=38, mobile='13866778899')
+f3(user='骆昊', nianling=38, mobile='13866778899')
